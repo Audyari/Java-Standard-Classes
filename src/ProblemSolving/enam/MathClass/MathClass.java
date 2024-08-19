6a65a92b0575a3ea59da67850e63944a6e8f6b0b
Audyari Wiyono <audy123ari@gmail.com>
2024-08-19 10:28:49 +0700

    6. MathClass

---
+++ src/ProblemSolving/enam/MathClass/MathClass.java
@@ -0,0 +1,13 @@
+package ProblemSolving.enam.MathClass;
+
+public class MathClass {
+    public static void main(String[] args) {
+        var testMin = Math.min(10,100);
+        System.out.println(testMin);
+
+        var testMax = Math.max(10,100);
+        System.out.println(testMax);
+
+        System.out.println(Math.PI);
+    }
+}
