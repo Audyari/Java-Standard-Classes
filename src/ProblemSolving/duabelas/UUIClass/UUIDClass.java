7271d17353e762519e3344590e04b9d1c367feaf
Audyari Wiyono <audy123ari@gmail.com>
2024-08-20 13:43:05 +0700

    12. UUID Class

---
+++ src/ProblemSolving/duabelas/UUIClass/UUIDClass.java
@@ -0,0 +1,15 @@
+package ProblemSolving.duabelas.UUIClass;
+
+import java.util.UUID;
+
+public class UUIDClass {
+    public static void main(String[] args) {
+
+        for(var i =0;i<=10;i++){
+            UUID testKodeUnik = UUID.randomUUID();
+            System.out.println(testKodeUnik);
+        }
+
+
+    }
+}
